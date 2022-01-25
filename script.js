aec0d99988c76d8c45b020a531a43e918bc4b237
WalterJunior <walterjuniordossanto@gmail.com>
2022-01-25 09:10:38 -0300

    Loop

---
+++ script.js
@@ -0,0 +1,14 @@
+for(let i = 1; i <= 10;  i++){
+    console.log(i)
+}
+let a = 10;
+while(a >= 1){
+    console.log(a)
+    a--
+}
+for (let b = 1; b <= 100; b = b+2) {
+    console.log(b)
+}
+for (let b = 0; b <= 100; b = b+2) {
+    console.log(b)
+}
